eba67970c19f38dae7a25aa9dd8c67ed36fc3ec0
Shreya <54427494+dhandashreya@users.noreply.github.com>
2024-08-10 13:41:59 -0600

    Update Two Sum.py

--- Easy/Two Sum.py
+++ Easy/Two Sum.py
@@ -1,3 +1,9 @@
+# LeetCode Problem: Two Sum
+# problem link : https://leetcode.com/problems/two-sum/description/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
 #solution 1 , this solution has a time complexity of O(N^2) in the worst-case scenario. 
 #This inefficiency arises because the in operator and index method, used within the loop, 
 # both iterate through the list, potentially leading to quadratic time complexity.
