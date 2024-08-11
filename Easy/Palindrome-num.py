67e944cfc072b620cf19476ad9acd43421a05b40
Shreya <54427494+dhandashreya@users.noreply.github.com>
2024-08-11 09:30:04 -0600

    Update Palindrome-num.py

--- Easy/Palindrome-num.py
+++ Easy/Palindrome-num.py
@@ -23,3 +23,16 @@ class Solution:
                     else:
                         return False
         return True
+
+# solution 2
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or (x!=0 and x%10 == 0):
+            return False
+        rev_num = 0
+
+        while x>rev_num:
+            rev_num = rev_num*10 + x%10
+            x //= 10
+
+        return x == rev_num or x == rev_num // 10
